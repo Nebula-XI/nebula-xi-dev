aaaf26242c72775cdbb1f19471d34e81eb9963fe
Alexander Chernenko <chernenko.a@insys.ru>
2021-05-14 16:10:25 +0300

    Refactoring

--- tests/test_dev_spi/test_dev_spi.cpp
+++ tests/test_dev_spi/test_dev_spi.cpp
@@ -24,21 +24,7 @@ SOFTWARE.
 
 */
 
-#pragma once
-
-#include "dev_base.h"
-
-namespace nebulaxi {
-
-class dev_axi_sdram final {
-    uint64_t m_axi_offset {};
-
-public:
-    dev_axi_sdram() = default;
-    dev_axi_sdram(uint64_t axi_offset)
-        : m_axi_offset { axi_offset }
-    {
-    }
-};
-
+int main(int argc, char* argv[])
+{
+    return 0;
 }
